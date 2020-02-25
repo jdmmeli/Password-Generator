cd5d2d8d4035bb9b255e88aa707af860cdb93a3b
Jason Meli <jdmmeli@yahoo.com>
2020-02-24 20:49:53 -0500

    click button for prompt

---
+++ assets/javascript.js
@@ -0,0 +1,6 @@
+var passwordLetters = ["a", "b", "c", "d", "e", "f", "g", "h"];
+var passwordNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var passwordSpecial = ["!", "#", "$", "%", "&", "*"];
+function userCriteria () {
+    var user = prompt("Please choose length of password")
+}
