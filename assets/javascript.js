74b340fea3bfbfec30096843adb396d07554ff99
Jason Meli <jdmmeli@yahoo.com>
2020-02-27 14:16:43 -0500

    call function generate

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,27 +1,50 @@
-var password = {
-    "lowercase": "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z",
-    "uppercase": "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z",
-    "numbers": "1,2,3,4,5,6,7,8,9",
-    "special": "!,@,#,$,%,&,*",
-    "length": "length.value"
-}
-
-
-  
+var length = 0;
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var special = "!@#$%^&*()";
 
 function userCriteria () {
-    var length = prompt("Please choose length of password, between 8-128 characters");
-    console.log(length);
+    var length = prompt("Please choose length of password, between 8-125 characters");
+    if (length < 8) {
+        prompt("Must have at least 8 characters");    
+    }
+    else if (length > 125) {
+        prompt("Must be 125 characters or less")
+    }
+      console.log(length);
+    
 
-    var lowercase = confirm("Click ok to include lowercase letters");
-    console.log(lowercase);
+    var lowerCase = confirm("Click ok to include lowercase letters");
 
-    var uppercase = confirm("CLick ok to include uppercase letters");
-    console.log(uppercase);
+    console.log(lowerCase)
+
+    var upperCase = confirm("CLick ok to include uppercase letters");
+    console.log(upperCase);
 
     var numbers = confirm("CLick ok to include numbers");
     console.log(numbers);
 
     var special = confirm("click ok to include special characters");
     console.log(special);
+
+    var userCriteria = confirm("You have chosen " + length +  lowercase +  uppercase +  numbers +  special)
+    console.log(userCriteria)
 }
+
+generate ()
+
+ 
+
+ function generate() {
+
+  passwordValues = "!@#$%^&*()abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
+  password = ""
+
+    for (var i = 0; i <= 125; i ++) {
+        
+   passsword = password + userCriteria.charAt(Math.floor(Math.random() * Math.floor(password.length - 1)))
+    console.log(password)
+ }
+}  
+
